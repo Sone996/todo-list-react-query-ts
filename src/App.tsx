133a3e6255d1de04663c277fcf5057ab9353dbbe
Nebojša Ilić <nebojsa.ilic@factoryww.com>
2021-08-12 12:25:36 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="bg-reactBlue text-white">hjxczhjxczhjxcxczjkxzckj</div>
+    </div>
+  );
+}
+
+export default App;
